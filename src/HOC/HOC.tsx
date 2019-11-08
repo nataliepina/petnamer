287a0d2be41f7f7ed6cc6a648905c30d611df7c5
nataliepina <natalie.a.pina@gmail.com>
2019-11-08 11:00:19 -0600

    temporary fix for HOC

--- src/HOC/HOC.tsx
+++ src/HOC/HOC.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+// import React from 'react'
 
-// @ts-ignore-start
-const withSecret = function withSecret(WrappedComponent: any) {
-  return class extends React.Component {
-    render() {
-      return (
-        <div>
-          <WrappedComponent
-            {...this.props}
-            secret={'Welcome to my secret portal!'}
-          />
-        </div>
-      )
-    }
-  }
-}
+// // @ts-ignore-start
+// const withSecret = function withSecret(WrappedComponent: any) {
+//   return class extends React.Component {
+//     render() {
+//       return (
+//         <div>
+//           <WrappedComponent
+//             {...this.props}
+//             secret={'Welcome to my secret portal!'}
+//           />
+//         </div>
+//       )
+//     }
+//   }
+// }
 
-export default withSecret
-// @ts-ignore-end
+// export default withSecret
+// // @ts-ignore-end
