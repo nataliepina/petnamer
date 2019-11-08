faefc1f009d2c9aa6339b785a5ec0e79b8041cfa
nataliepina <natalie.a.pina@gmail.com>
2019-11-08 11:01:15 -0600

    Update comments

--- src/components/PetPortal.tsx
+++ src/components/PetPortal.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import usePortal from 'react-useportal'
-import withSecret from '../HOC/HOC'
+// import withSecret from '../HOC/HOC'
 
-const DisplaySecret = (props: any) => <div>The secret is... {props.secret}</div>
+// const DisplaySecret = (props: any) => <div>The secret is... {props.secret}</div>
 
-export const WrappedComponent = withSecret(DisplaySecret)
+// export const WrappedComponent = withSecret(DisplaySecret)
 
 const PetPortal = (props: any) => {
   const {openPortal, closePortal, isOpen, Portal} = usePortal()
